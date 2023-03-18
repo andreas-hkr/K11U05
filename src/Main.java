f77632ceae3d8e944ce8cf8c0d1c7b7cc6467ba2
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 10:44:16 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skriv en egen version av Arrays.equals metoden
+        //  - Längden måste vara lika
+        //  - Elementen på respektive index måste vara samma
+
+
+    }
+}
